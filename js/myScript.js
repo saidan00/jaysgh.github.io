1407817760fdfab118ecbd451226c73b3fffcabe
saidan00 <huysgh94@gmail.com>
2018-10-04 22:42:42 +0700

    big update

---
+++ js/myScript.js
@@ -0,0 +1,8 @@
+function toggleHeaderRightMenu() {
+    var x = document.getElementById("header-right");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}

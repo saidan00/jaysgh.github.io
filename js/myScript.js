f999d7959bd69759770c18d773be12b7afb3a6f4
saidan00 <huysgh94@gmail.com>
2018-10-04 23:44:16 +0700

    fix

--- js/myScript.js
+++ js/myScript.js
@@ -2,7 +2,22 @@ function toggleHeaderRightMenu() {
     var x = document.getElementById("header-right");
     if (x.style.display === "none") {
         x.style.display = "block";
-    } else {
+    }
+    else {
         x.style.display = "none";
     }
 }
+function showHeaderRightMenu(scrsz) {
+  //scrsz: screen size
+  var x = document.getElementById("header-right");
+  if (scrsz.matches) {
+    x.style.display = "none";
+  }
+  else {
+    x.style.display = "block";
+  }
+}
+
+var scrsz = window.matchMedia("(max-width: 712px)")
+showHeaderRightMenu(scrsz)
+scrsz.addListener(showHeaderRightMenu)
